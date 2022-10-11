d0fab828a5a5500a881ccdadcb63704bd71c1349
Alexey Ponomarev <ygonnik0061@gmail.com>
2022-10-11 18:26:43 +0500

    chat

--- frontend/src/pages/chat.js
+++ frontend/src/pages/chat.js
@@ -3,22 +3,32 @@ import { fetchMessages } from '../http/userAPI';
 import {Context} from '../index'
 
 function ChatPage() {
-    const {user} = useContext(Context);
-    const interlocutors = useState([])
+  const {user} = useContext(Context);
+  const interlocutors = useState([])
 
-    const SetChat = (data) => {
-        user.setMessages(data)
-        for (let message in user.messages) {
-            if (!interlocutors.includes(message.user2Id)) {
-                interlocutors.push(message.user2Id)
-            }
-        }
+  const SetChat = (data) => {
+    user.setMessages(data)
+    for (let message in user.messages) {
+      if (!interlocutors.includes(message.user2Id)) {
+          interlocutors.push(message.user2Id)
+      }
     }
+  }
 
     useEffect(() => {
         fetchMessages(user.user.id).then(data => SetChat(data))
       // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
+    useEffect(() => {
+      const socket = new WebSocket('ws://localhost:3001/')
+      socket.onopen = () => {
+        socket.send(JSON.stringify({
+          id:user.user.id,
+          method:"connection"
+        }))
+      }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]) 
     return (
     <div class="container mt-3">
         <div class="row clearfix">
@@ -29,37 +39,31 @@ function ChatPage() {
                             <li class="clearfix">
                                 <div class="about">
                                     <div class="name">Vincent Porter</div>
-                                    <div class="status"> <i class="fa fa-circle offline"></i> left 7 mins ago </div>                                            
                                 </div>
                             </li>
                             <li class="clearfix active">
                                 <div class="about">
                                     <div class="name">Aiden Chavez</div>
-                                    <div class="status"> <i class="fa fa-circle online"></i> online </div>
                                 </div>
                             </li>
                             <li class="clearfix">
                                 <div class="about">
                                     <div class="name">Mike Thomas</div>
-                                    <div class="status"> <i class="fa fa-circle online"></i> online </div>
                                 </div>
                             </li>                                    
                             <li class="clearfix">
                                 <div class="about">
                                     <div class="name">Christian Kelly</div>
-                                    <div class="status"> <i class="fa fa-circle offline"></i> left 10 hours ago </div>
                                 </div>
                             </li>
                             <li class="clearfix">
                                 <div class="about">
                                     <div class="name">Monica Ward</div>
-                                    <div class="status"> <i class="fa fa-circle online"></i> online </div>
                                 </div>
                             </li>
                             <li class="clearfix">
                                 <div class="about">
                                     <div class="name">Dean Henry</div>
-                                    <div class="status"> <i class="fa fa-circle offline"></i> offline since Oct 28 </div>
                                 </div>
                             </li>
                         </ul>
