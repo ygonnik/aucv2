d998f6b3ca6f5890b78540c177081e7d9dd0eec7
Alexey Ponomarev <ygonnik0061@gmail.com>
2022-11-26 15:24:20 +0500

    local

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,11 +5,11 @@ module.exports = new Sequelize(
     process.env.POSTGRES_PASSWORD,
     {
         dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                rejectUnauthorized: false
-            }
-        },
+        // dialectOptions: {
+        //     ssl: {
+        //         rejectUnauthorized: false
+        //     }
+        // },
         host: process.env.POSTGRES_HOST,
         port: process.env.POSTGRES_PORTDB_PORT
     }
